6f1b69edaec84ae6a6d1363aa54154759704e25c
Leonardo Oliveira <leonardo.dias.oliveira@gmail.com>
2023-11-14 21:08:07 -0500

    First Commit

---
+++ src/main/java/com/example/ldo/demo/DemoSpringBootGithubActionApplication.java
@@ -0,0 +1,14 @@
+package com.example.ldo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoSpringBootGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ldo/demo/DemoSpringBootGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ldo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
